1ace226bcb5a71192c13df7dba19bee7273e11df
Nick <pattison.nick@gmail.com>
2019-11-21 12:28:28 -0500

    Added project working files

---
+++ Assets/-MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+
+    [Tooltip("A list of all the levels to display on this screen.")]
+    public LevelInfo[] levels;
+
+
+    EventSystem events;
+
+    void Start() {
+        events = GetComponentInChildren<EventSystem>();
+        
+    }
+    public void LoadLevel(string levelName) {
+        SceneManager.LoadScene(levelName);
+    }
+    public void ExitGame() {
+        Application.Quit();
+    }
+    void Update() {
+        Focus();
+    }
+    void Focus() {
+        if (events == null) return;
+        if (events.currentSelectedGameObject != null) return;
+        if (events.firstSelectedGameObject == null) return;
+        events.SetSelectedGameObject(events.firstSelectedGameObject);
+    }
+}
