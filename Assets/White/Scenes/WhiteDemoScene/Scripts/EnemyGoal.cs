0523dbd64a788c9a8574236219cab3279d6dee62
Unknown <whitel24@ferris.edu>
2019-12-12 10:10:00 -0500

    Added doc comments

--- Assets/White/Scenes/WhiteDemoScene/Scripts/EnemyGoal.cs
+++ Assets/White/Scenes/WhiteDemoScene/Scripts/EnemyGoal.cs
@@ -6,11 +6,24 @@ using UnityEngine.UI;
 
 namespace White
 {
+    /// <summary>
+    /// This class handles the behavior of the base.
+    /// </summary>
     public class EnemyGoal : MonoBehaviour
     {
+        /// <summary>
+        /// The image that displays the base's health.
+        /// </summary>
         public Image healthBar;
 
+        /// <summary>
+        /// The amount of health the base has.
+        /// </summary>
         float health = 100;
+
+        /// <summary>
+        /// Determines if the base is dead.
+        /// </summary>
         public bool isDead
         {
             get
@@ -19,23 +32,33 @@ namespace White
             }
         }
 
+        /// <summary>
+        /// Updates the health bar when the base takes damage.
+        /// </summary>
         void Update()
         {
             if (healthBar) healthBar.fillAmount = health / 100;
 
             if (isDead) Explode();
-        }
+        } // ends the Update() function
 
+        /// <summary>
+        /// This function destroys the game object.
+        /// </summary>
         private void Explode()
         {
             // TODO: spawn particles
             // TODO: play audio
             Destroy(gameObject);
-        }
+        } // ends the Explode() function
 
+        /// <summary>
+        /// This function takes away the base's health when it takes damage.
+        /// </summary>
+        /// <param name="amount">The amount of health the base has.</param>
         public void TakeDamage(float amount)
         {
             health -= amount;
-        }
-    }
-}
+        } // ends the TakeDamage() function
+    } // ends the EnemyGoal() function
+} // ends the White namespace
--- Assets/White/Scenes/WhiteDemoScene/Scripts/Tower.cs
+++ Assets/White/Scenes/WhiteDemoScene/Scripts/Tower.cs
@@ -5,33 +5,58 @@ using UnityEngine;
 
 namespace White
 {
+    /// <summary>
+    /// This class defines the behavior of the towers.
+    /// </summary>
     public class Tower : MonoBehaviour
     {
+        /// <summary>
+        /// This list stores the enemies seen by the tower.
+        /// </summary>
         List<EnemyController> enemies = new List<EnemyController>();
 
+        /// <summary>
+        /// The prefab that is being used as a projectile.
+        /// </summary>
         public Projectile prefabProjectile;
 
+        /// <summary>
+        /// The target that the tower is currently attacking.
+        /// </summary>
         public Transform attackTarget { get; private set; }
-
-        void Start()
-        {
-
-        }
         
+        /// <summary>
+        /// This function updates each tower spawned.
+        /// </summary>
         void Update()
         {
+            GetClosestEnemy();
+        } // ends the Update() function
 
-        }
-
+        /// <summary>
+        /// This function finds the enemy closest to the tower.
+        /// </summary>
+        /// <returns>The enemy closest to the tower.</returns>
         EnemyController GetClosestEnemy()
         {
+            /// <summary>
+            /// Which enemy is the closest to the tower.
+            /// </summary>
             EnemyController result = null;
+
+            /// <summary>
+            /// The minimum distance the enemy can be from the tower.
+            /// </summary>
             float minDis = 0;
 
             // find closest
             foreach(EnemyController e in enemies)
             {
-                float dis = (e.transform.position - transform.position).magnitude; // distance from tower to enemy
+                /// <summary>
+                /// The distance from the tower to the enemy.
+                /// </summary>
+                float dis = (e.transform.position - transform.position).magnitude;
+
                 if (dis < minDis)
                 {
                         result = e;
@@ -40,43 +65,81 @@ namespace White
             }
 
             return result;
-        }
+        } // ends the GetClosestEnemy() function
 
+        /// <summary>
+        /// This function makes the tower choose a random enemy to fire at.
+        /// </summary>
+        /// <returns>The enemy to fire at.</returns>
         EnemyController GetRandomEnemy()
         {
             if (enemies.Count <= 0) return null;
 
+            /// <summary>
+            /// The enemy to fire at.
+            /// <summary>
             int index = UnityEngine.Random.Range(0, enemies.Count);
 
             return enemies[index];
-        }
+        } // ends the GetRandomEnemy() function
 
+        /// <summary>
+        /// This function determines if an enemy has entered the tower's trigger.
+        /// </summary>
+        /// <param name="collider">The tower's collider.</param>
         void OnTriggerEnter(Collider collider)
         {
-            print("something entered my trigger");
-            EnemyController e = collider.GetComponent<EnemyController>();
-            if (e != null) enemies.Add(e);
-            ShootProjectile();
-        }
+            //print("something entered my trigger");
 
+            /// <summary>
+            /// The enemy that can enter the tower's trigger.
+            /// </summary>
+            EnemyController e = collider.GetComponent<EnemyController>();
+
+            if (e != null) enemies.Add(e);
+        } // ends the OnTriggerEnter() function
+
+        /// <summary>
+        /// This function determines if the enemy has left the tower's trigger.
+        /// </summary>
+        /// <param name="collider">The tower's collider.</param>
         void OnTriggerExit(Collider collider)
         {
-            print("something exited my trigger");
-            EnemyController e = collider.GetComponent<EnemyController>();
-            if (e != null) enemies.Remove(e);
-        }
+            //print("something exited my trigger");
 
+            /// <summary>
+            /// The enemy that can exit the tower's trigger.
+            /// </summary>
+            EnemyController e = collider.GetComponent<EnemyController>();
+
+            if (e != null) enemies.Remove(e);
+        } // ends the OnTriggerExit() function
+
+        /// <summary>
+        /// This function determines the vector from the tower to the attack target.
+        /// </summary>
+        /// <returns>The vector from the tower to the attack target.</returns>
         public Vector3 VectorToAttackTarget()
         {
             return attackTarget.position - transform.position;
-        }
+        } // ends the VectorToAttackTarget()
 
+        /// <summary>
+        /// This function makes the tower shoot a projectile at the enemy.
+        /// </summary>
         private void ShootProjectile()
         {
+            /// <summary>
+            /// Instantiates the projectile.
+            /// </summary>
             Projectile newProjectile = Instantiate(prefabProjectile, transform.position, Quaternion.identity);
 
+            /// <summary>
+            /// The direction for the tower to shoot the projectile.
+            /// </summary>
             Vector3 dir = (VectorToAttackTarget() + UnityEngine.Random.insideUnitSphere * 5).normalized;
+
             newProjectile.Shoot(gameObject, dir);
-        }
-    }
-}
+        } // ends the ShootProjectile() function
+    } // ends the Tower class
+} // ends the White namespace
